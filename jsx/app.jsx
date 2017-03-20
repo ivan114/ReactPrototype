2eb62b2da4919afdd70bdac53cabc37ef82397cf
Ivan <ivan114@me.com>
2017-03-20 15:54:31 +0800

    react-bootstrap & ie8 polyfill added

--- jsx/app.jsx
+++ jsx/app.jsx
@@ -1,11 +1,19 @@
-
 class Greeting extends React.Component {
   render() {
-    return <h1>Hello World!</h1>;
+    return (
+        <ReactBootstrap.Jumbotron>
+            <h1>Hello, world!</h1>
+            <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+            <p><ReactBootstrap.Button bsStyle="primary">Learn more</ReactBootstrap.Button></p>
+        </ReactBootstrap.Jumbotron>
+    );
   }
 }
 
 ReactDOM.render(
-  <Greeting />,
+    <ReactBootstrap.Grid>
+        <Greeting />
+    </ReactBootstrap.Grid>
+  ,
   document.getElementById('root')
 );
